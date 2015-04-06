08c2e598d5126ce722ca1e9da9ae462813e32004
Romain Reuillon <romain.reuillon@iscpif.fr>
2015-04-06 15:26:00 +0200

    Update scala verison.

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "myopenmoleplugin"
 
 version := "1.0"
 
-scalaVersion := "2.11.2"
+scalaVersion := "2.11.6"
 
 osgiSettings
 
--- src/main/scala/myopenmoleplugin/Hello.scala
+++ src/main/scala/myopenmoleplugin/Hello.scala
@@ -1,6 +1,9 @@
 package myopenmoleplugin
 
 object Hello {
+  case class Result(value: Int)
+
   def world(i: Int) = i * 2
+  def typedWorld(i: Int) = Result(world(i))
 }
 
