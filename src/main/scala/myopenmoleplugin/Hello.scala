6b582569cb940c096ce1208d962699478daab412
Romain Reuillon <romain.reuillon@iscpif.fr>
2015-04-07 13:49:00 +0200

    Use a trait to make the usedClasses work in openmole.

--- src/main/scala/myopenmoleplugin/Hello.scala
+++ src/main/scala/myopenmoleplugin/Hello.scala
@@ -1,9 +1,14 @@
 package myopenmoleplugin
 
-object Hello {
-  case class Result(value: Int)
+object Hello extends Hello
 
+case class Result(value: Int)
+
+trait Hello {
   def world(i: Int) = i * 2
   def typedWorld(i: Int) = Result(world(i))
 }
+
+
+
 
